9030262f5412387260d78b4eac6f945a736d8d34
leeher1123 <leeher1123@naver.com>
2022-03-22 19:27:47 +0900

    added our sign in component as well as firebase utils and brought in firebase libraries

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,50 @@
+import { initializeApp } from 'firebase/app';
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from 'firebase/auth';
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBqjlRbpL9qkoQ8q9vNdX1P_xEcGCHByoI',
+  authDomain: 'crwn-db-556d3.firebaseapp.com',
+  projectId: 'crwn-db-556d3',
+  storageBucket: 'crwn-db-556d3.appspot.com',
+  messagingSenderId: '100311632209',
+  appId: '1:100311632209:web:054c0f5181c5d704e22b70',
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: 'select_account',
+});
+
+export const auth = getAuth();
+export const db = getFirestore();
+
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid);
+  const userSnapshot = await getDoc(userDocRef);
+
+  if (!userSnapshot.exists()) {
+    // user data 가 존재하지 않으면 아래 코드가 실행된다.
+    const { displayName, email } = userAuth;
+    const createdAt = new Date(); // 사용자가 언제 로그인하는지 알 수 있음
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log('error creating the user', error.message);
+    }
+  }
+  return userDocRef;
+};
